b907105cba900bd8192719453ee7de8d6257b6d0
sebres <info@sebres.de>
2019-12-21 06:03:24 +0100

    initial (tcl and python program to calculate/estimate a chance of 2 cards "collision")

---
+++ id-hash-colprob.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+def _opt(i, dflt=None):
+  return int(sys.argv[i]) if len(sys.argv) > i else dflt
+
+K = _opt(1);  # K cards in deck
+k = _opt(2);  # k cards we will take from the deck
+N = _opt(3);  # N variants of different card types (we can "hash" at all)
+TM = _opt(4); # time to run estimation (iterate)
+
+if K % N:
+  print("Warning: n is not decimal, we have rest by %s / %s (%s %% %s != 0), estimated and calculated probabilities may vary" % (
+    K, N, K, N))
+
+n = K / N
+print("k = %s cards from K = %s max, by sieve N = 1..%d, types repeated n = %d times:" % (k, K, N, n))
+
+# calculate (recursive probability tree):
+
+def calcp(d, k):
+  global K, n
+  v = float(d*n-d)/(K-d)
+  if k > 2:
+    v += float(K-n*d)/(K-d) * calcp(d+1, k-1)
+  return v
+print("Calc-P(collision)=%s" % calcp(1, k))
+
+# estimation (simulate playing):
+if TM:
+  import time, random
+  # fill the array NewDck with a card deck (K cards in deck, repeats K/N times of N variants of the cards):
+  NewDck = []
+  v = 0
+  i = 0
+  while i < K:
+    v += 1
+    NewDck.append(v)
+    if v >= N: v = 0
+    i += 1
+  # print(NewDck)
+
+  # run estimation cycle:
+  col = {}
+  col['yes'] = 0
+  col['no'] = 0
+
+  endTM = time.time() + float(TM) / 1000
+  while time.time() < endTM:
+    Dck = list(i for i in NewDck)
+    Tk = {}
+    # take k cards and check for a collision:
+    fnd = 0
+    x = 1
+    while x <= k:
+      while 1:
+        i = random.randint(0, K-1); # random index in deck
+        t = Dck[i]
+        if t != -1: break; # unused card
+      if t in Tk:
+        fnd = 1; break;   # same type of card (collision) found
+      Tk[t] = 1;          # add new know type in subset of cards
+      Dck[i] = -1;        # mark card as used in deck
+      x += 1
+    if fnd: # incr count of collision / success
+      col['yes'] += 1
+    else:
+      col['no'] += 1
+
+  col['iter'] = col['no']+col['yes']
+  print("Estm-P(collision)=%s" % (float(col['yes'])/col['iter'],))
+  print("Results: %s" % col)
---
+++ id-hash-colprob.tcl
@@ -0,0 +1,80 @@
+#!/usr/bin/env tclsh
+
+if {![llength $argv]} {
+  puts "usage: $argv0 K k N ?TM?"
+  return
+}
+
+set K [lindex $argv 0]   ; # K cards in deck
+set k [lindex $argv 1]   ; # k cards we will take from the deck
+set N [lindex $argv 2]   ; # N variants of different card types (we can "hash" at all)
+set TM [lindex $argv 3]  ; # time to run estimation (iterate)
+
+if {$K % $N} {
+  puts "Warning: n is not decimal, we have rest by $K / $N ($K % $N != 0), estimated and calculated probabilities may vary"
+}
+
+set n [expr {$K / $N}]
+puts "k = $k cards from K = $K max, by sieve N = 1..$N, types repeated n = $n times:"
+
+# calculate (recursive probability tree):
+
+# example to calculate coll-probality by k = 4:
+#
+# puts Calc-P(collision)=[expr {
+#   (1*$n-1.0)/($K-1) + ($K-$n*1.0)/($K-1) * ((2*$n-2.0)/($K-2) + ($K-$n*2.0)/($K-2) * (3*$n-3.0)/($K-3))
+# }]
+proc calcp {d k} {
+  upvar K K n n
+  set v [expr {double($d*$n-$d)/($K-$d)}]
+  if {$k > 2} {
+    set v [expr {$v + (double($K-$n*$d)/($K-$d) * [calcp [expr {$d+1}] [expr {$k-1}]])}]
+  }
+  set v
+}
+puts Calc-P(collision)=[calcp 1 $k]
+
+# estimation (simulate playing):
+if {$TM ne {}} {
+
+  # fill the array NewDck with a card deck (K cards in deck, repeats K/N times of N variants of the cards):
+  set NewDck {}
+  set v 0
+  for {set i 0} {$i < $K} {incr i} {
+    dict set NewDck $i [incr v]
+    if {$v >= $N} {set v 0}
+  }
+  # puts $NewDck
+
+  # run estimation cycle:
+  set col(yes) 0
+  set col(no) 0
+  set tr [timerate {
+    set Dck $NewDck
+    set Tk {}
+    # take k cards and check for a collision:
+    set fnd 0
+    for {set x 1} {$x <= $k} {incr x} {
+      while 1 {
+        set i [expr {int(rand()*$K)}]; # random index in deck
+        if {[set t [dict get $Dck $i]] != -1} break; # unused card
+      }
+      if {[dict exists $Tk $t]} {set fnd 1; break}; # same type of card (collision) found
+      dict set Tk $t _;   # add new know type in subset of cards
+      dict set Dck $i -1; # mark card as used in deck
+    }
+    if {$fnd} { # incr count of collision / success
+      incr col(yes)
+    } else {
+      incr col(no)
+    }
+  } $TM]
+
+  set col(iter) [expr { $col(no)+$col(yes) }]
+  puts Estm-P(collision)=[expr { double($col(yes))/$col(iter) }]
+  puts "Results: [array get col]"
+  puts $tr
+  # puts $Tk
+  # puts $Dck
+}
+
